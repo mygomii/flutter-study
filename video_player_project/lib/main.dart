fd303d0bd573e71ea827a710221b5666f537959d
mijeongko <komijeong91@gmail.com>
2023-12-21 14:03:08 +0900

    섹션 18. [프로젝트] [★★☆☆☆] 동영상 플레이어

---
+++ video_player_project/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:video_player_project/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
